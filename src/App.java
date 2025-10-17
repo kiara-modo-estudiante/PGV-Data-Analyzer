e7484a66181b460606b4d46dc990fc4fbca3354b
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-10-17 20:15:55 +0100

    feat(app): integrate CsvReader to read products and sales CSV files

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import net.salesianos.utils.CsvReader;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        final String PRODUCTS_CSV_PATH = "./src/data/input/products.csv";
+        final String SALES_CSV_PATH = "./src/data/input/sales.csv";
+
+        CsvReader fileReader = new CsvReader();
+        fileReader.readCSV(PRODUCTS_CSV_PATH);
+
     }
 }
